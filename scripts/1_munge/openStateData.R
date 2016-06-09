766567aadff28992ff3ec5f58244cdb43efe5d50
Laura DeCicco <ldecicco@usgs.gov>
2016-06-09 12:20:14 -0500

    Changed TB to Turb because there's an optical property TB.

--- scripts/1_munge/openStateData.R
+++ scripts/1_munge/openStateData.R
@@ -10,22 +10,35 @@ library(smwrBase)
 raw.lab.path <- "raw_data/field_data"
 cached.path <- "cached_data"
 
-#MI:
-file.mi <- "GLPF.NWIS.MI.xlsx"
-data.mi <- read_excel(file.path(raw.lab.path,file.mi))
-
-#NY:
-file.ny <- "NY_CHEMICAL_PARAMETER_DATA.xlsx"
-data.ny <- read_excel(file.path(raw.lab.path,file.ny), na = "X")
-data.ny[,4:8] <- sapply(data.ny[,4:8], function(x) as.numeric(gsub(",","", x)))
-
-#WI:
+###############################################################
+# WI:
+###############################################################
 file.wi <- "KK_Compiled_Sensor Data20160513.csv"
 data.wi <- setDF(fread(file.path(raw.lab.path,file.wi)))
+
+data.wi$SAMPLE_START_DT <- data.wi$SampleCollectionDateTime
+# data.wi$SAMPLE_START_DT[is.na(data.wi$SAMPLE_START_DT) | data.wi$SAMPLE_START_DT == ""] <- data.wi$TIMESTAMP[is.na(data.wi$SAMPLE_START_DT) | data.wi$SAMPLE_START_DT == ""]
+data.wi$SAMPLE_START_DT <- parse_date_time(data.wi$SAMPLE_START_DT, orders = c("mdY HS", "Ymd H:M:S"))
+# data.wi$SAMPLE_START_DT[is.na(data.wi$SAMPLE_START_DT)] <- parse_date_time(paste(data.wi$Sample_Collection_Date[is.na(data.wi$SAMPLE_START_DT)], data.wi$Sensor_Start_Time[is.na(data.wi$SAMPLE_START_DT)]), orders = c("mdY H:M:S", "mdY"))
+
+data.wi.munged <- rename(data.wi, 
+                         WT = Temp,
+                         FieldID = Station_Name) %>%
+  mutate(DO = as.numeric(NA)) %>%
+  select(SAMPLE_START_DT, FieldID, state, WT, DO, Turb, SC, pH,UVch1,UVch2,UVch3) %>%
+  filter(!(FieldID %in% c("BLK-DI","FLD-BLK","EMPTY"))) 
+
+data.wi.munged <- data.wi.munged %>%
+  filter(state == "WI")
+
+saveRDS(data.wi.munged, file.path(cached.path,"dataWI.rds"))
 
 ###############################################################
 # MI:
 ###############################################################
+file.mi <- "GLPF.NWIS.MI.xlsx"
+data.mi <- read_excel(file.path(raw.lab.path,file.mi))
+
 data.mi <- rename(data.mi,
                   SiteID = `Station ID`,
                   fieldID = `Field ID`,
@@ -41,82 +54,53 @@ data.mi.wide <- setDT(data.mi) %>%
           PARM_NM, value.var = "RESULT_VA") %>%
   setDF()
 
-# dupRows <- c(which(duplicated(data.mi.wide$SAMPLE_START_DT)),
-#              which(duplicated(data.mi.wide$SAMPLE_START_DT,fromLast = TRUE)))
-# dupRows <- dupRows[order(dupRows)]
-# x <- data.mi.wide[dupRows,] %>%
-#   arrange(SAMPLE_START_DT)
-
 data.mi.wide <- rename(data.mi.wide, 
                        Station_Name=STATION_NM,
                        FieldID=fieldID) %>%
   mutate(SC = `Specific cond at 25C`,
          WT = `Temperature, water`,
-         TB = `Turbidity, Nephelom`,
+         Turb = `Turbidity, Nephelom`,
          DO = `Dissolved oxygen`,
          state = "MI") %>%
-  select(SAMPLE_START_DT, FieldID, state, WT, DO, TB, SC, pH) 
+  select(SAMPLE_START_DT, FieldID, state, WT, DO, Turb, SC, pH) 
 
 saveRDS(data.mi.wide, file.path(cached.path,"dataMI.rds"))
-
-###############################################################
-# NY:
-###############################################################
-data.ny$SAMPLE_START_DT <- fast_strptime(paste(data.ny$Date,data.ny$Time), "%m/%d/%Y %H%M", tz = "UTC", lt=FALSE)
-data.ny$state <- "NY" 
-
-data.ny <- rename(data.ny,
-                  DO = D.O.)
-
-###############################################################
-# WI:
-###############################################################
-data.wi$SAMPLE_START_DT <- data.wi$SampleCollectionDateTime
-# data.wi$SAMPLE_START_DT[is.na(data.wi$SAMPLE_START_DT) | data.wi$SAMPLE_START_DT == ""] <- data.wi$TIMESTAMP[is.na(data.wi$SAMPLE_START_DT) | data.wi$SAMPLE_START_DT == ""]
-data.wi$SAMPLE_START_DT <- parse_date_time(data.wi$SAMPLE_START_DT, orders = c("mdY HS", "Ymd H:M:S"))
-# data.wi$SAMPLE_START_DT[is.na(data.wi$SAMPLE_START_DT)] <- parse_date_time(paste(data.wi$Sample_Collection_Date[is.na(data.wi$SAMPLE_START_DT)], data.wi$Sensor_Start_Time[is.na(data.wi$SAMPLE_START_DT)]), orders = c("mdY H:M:S", "mdY"))
-
-data.wi.munged <- rename(data.wi, 
-                         WT = Temp,
-                         TB = Turb,
-                         FieldID = Station_Name) %>%
-                  mutate(DO = as.numeric(NA)) %>%
-                  select(SAMPLE_START_DT, FieldID, state, WT, DO, TB, SC, pH,UVch1,UVch2,UVch3) %>%
-                  filter(!(FieldID %in% c("BLK-DI","FLD-BLK","EMPTY"))) 
-
-data.ny.opt.field <- data.wi.munged %>%
-  filter(state == "NY") %>%
-  select(SAMPLE_START_DT, FieldID, UVch1, UVch2, UVch3) 
-
-data.ny.join <- select(data.ny, SAMPLE_START_DT, FieldID, state, WT, DO, TB, SC, pH) 
-data.ny.join$FieldID[data.ny.join$FieldID =="WWRCTN"] <- "WWRCTN-2"
-data.ny.join$FieldID[data.ny.join$FieldID =="WWRCTE"] <- "WWRCTE-1or2"
-data.ny.join$FieldID[data.ny.join$FieldID =="WWRCTJ"] <- "WWRCTJ-1or2"
-
-data.ny.join$FieldID[nchar(data.ny.join$FieldID) == 6] <- paste0(data.ny.join$FieldID[nchar(data.ny.join$FieldID) == 6],"-1")
-
-data.ny.munged <- left_join(data.ny.join, data.ny.opt.field, by="FieldID") %>%
-  select(-SAMPLE_START_DT.y) %>%
-  rename(SAMPLE_START_DT=SAMPLE_START_DT.x)
-saveRDS(data.ny.munged, file.path(cached.path,"dataNY.rds"))
 
 data.mi.opt.field <- data.wi.munged %>%
   filter(state == "MI")
 
 saveRDS(data.mi.opt.field, file.path(cached.path,"dataMIfieldOpts.rds"))
 
-data.wi.munged <- data.wi.munged %>%
-  filter(state == "WI")
 
-saveRDS(data.wi.munged, file.path(cached.path,"dataWI.rds"))
+###############################################################
+# NY:
+###############################################################
+file.ny <- "NY_CHEMICAL_PARAMETER_DATA.xlsx"
+data.ny <- read_excel(file.path(raw.lab.path,file.ny), na = "X")
 
+data.ny[,4:8] <- sapply(data.ny[,4:8], function(x) as.numeric(gsub(",","", x)))
 
-# field.merge <- bind_rows(data.ny.munged, data.wi.munged) 
-# indexNA <- field.merge[,c("pH","DO","WT","TB","SC")]
-# indexNA <- which(rowSums(is.na(indexNA)) != ncol(indexNA))
-# 
-# field.merge <- field.merge[indexNA,]
-# 
-# saveRDS(field.merge, file.path(cached.path,"fieldMerge.rds"))
+data.ny$SAMPLE_START_DT <- fast_strptime(paste(data.ny$Date,data.ny$Time), "%m/%d/%Y %H%M", tz = "UTC", lt=FALSE)
+data.ny$state <- "NY" 
+
+data.ny <- rename(data.ny,
+                  DO = D.O.,
+                  Turb = TB)
+
+data.ny.opt.field <- data.wi.munged %>%
+  filter(state == "NY") %>%
+  select(SAMPLE_START_DT, FieldID, UVch1, UVch2, UVch3) 
+
+data.ny.join <- select(data.ny, SAMPLE_START_DT, FieldID, state, WT, DO, Turb, SC, pH) 
+data.ny.join$FieldID[data.ny.join$FieldID =="WWRCTN"] <- "WWRCTN-2"
+data.ny.join$FieldID[data.ny.join$FieldID =="WWRCTE"] <- "WWRCTE-1or2"
+data.ny.join$FieldID[data.ny.join$FieldID =="WWRCTJ"] <- "WWRCTJ-1or2"
+data.ny.join$FieldID[nchar(data.ny.join$FieldID) == 6] <- paste0(data.ny.join$FieldID[nchar(data.ny.join$FieldID) == 6],"-1")
+
+data.ny.munged <- left_join(data.ny.join, data.ny.opt.field, by="FieldID") %>%
+  select(-SAMPLE_START_DT.y) %>%
+  rename(SAMPLE_START_DT=SAMPLE_START_DT.x)
+
+saveRDS(data.ny.munged, file.path(cached.path,"dataNY.rds"))
 
 
