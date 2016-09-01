37c147773d3b2837fdea2ce0ac02dd65def46506
Laura DeCicco <ldecicco@usgs.gov>
2016-09-01 10:36:43 -0500

    Streamlined and more descriptive names

--- scripts/2_process/filterData.R
+++ scripts/2_process/filterData.R
@@ -2,50 +2,57 @@ library(dplyr)
 
 cached.path <- "cached_data"
 
+filterOptics <- function(df, dfabs, dffl, base.name, cached.path){
+  
+  wwGR <- unique(df$CAGRnumber)
+  wwGR <- wwGR[!(wwGR %in% c("x","N/A"))]
+  dfabs_filtered <- dfabs[,names(dfabs) %in% c("nm",wwGR)]
+  dffl_filtered <- dffl[,names(dffl) %in% c("exem",wwGR)]
+  
+  write.csv(dfabs_filtered, file = file.path(cached.path,"final",paste0("dfabs",base.name,".csv")), row.names = FALSE)
+  write.csv(dffl_filtered, file = file.path(cached.path,"final",paste0("dffl",base.name,".csv")), row.names = FALSE)
+  saveRDS(dfabs_filtered, file = file.path(cached.path,"final","rds",paste0("dfabs",base.name,".rds")))
+  saveRDS(dffl_filtered, file = file.path(cached.path,"final","rds",paste0("dffl",base.name,".rds")))
+  
+}
+
 filterData <- function(cached.path){
   df <- readRDS(file.path(cached.path,"merged","mergedData.rds"))
+  
+  dfabs <- readRDS(file.path(cached.path,"optics","dfabs.rds"))
+  dffl <- readRDS(file.path(cached.path,"optics","dffl.rds"))
   
   df_QA <-  filter(df, SampleType9regular2blank7replicate != 9)
   df_regular <- filter(df, SampleType9regular2blank7replicate == 9)
   
-  # df_auto <- filter(df_regular, VirusAutosampleorSewerGrab == "Autosample")
-  # 
+  write.csv(df_regular, file = file.path(cached.path,"final","summaryAll_noQA.csv"), row.names = FALSE)
+  saveRDS(df_regular, file = file.path(cached.path,"final","rds","summaryAll_noQA.rds"))
+  
+  filterOptics(df_regular, dfabs, dffl, "All_noQA", cached.path)
+  
+  # some day, we may want to separate auto samples:
+  # df_auto <- filter(df_regular, VirusAutosampleorSewerGrab == "Autosample") 
   # df_regular <- filter(df_regular, VirusAutosampleorSewerGrab != "Autosample")
+  
   wastewater.columns <- c("Sanitary","WWTP","WW Influent","Sanitary Grab")
   
   df_WW <- filter(df_regular, VirusAutosampleorSewerGrab %in% wastewater.columns)
-  
   df_regular <- df_regular[!(df_regular$VirusAutosampleorSewerGrab %in% wastewater.columns),]
   
-  write.csv(df_regular, file = file.path(cached.path,"final","mergedData.csv"), row.names = FALSE)
-  write.csv(df_WW, file = file.path(cached.path,"final","wwData.csv"), row.names = FALSE)
-  write.csv(df_QA, file = file.path(cached.path,"final","qaData.csv"), row.names = FALSE)
+  write.csv(df_regular, file = file.path(cached.path,"final","summaryNoWW_noQA.csv"), row.names = FALSE)
+  write.csv(df_WW, file = file.path(cached.path,"final","summaryWW.csv"), row.names = FALSE)
+  write.csv(df_QA, file = file.path(cached.path,"final","summaryQA.csv"), row.names = FALSE)
   
-  dfabs <- readRDS(file.path(cached.path,"optics","dfabs.rds"))
-  dffl <- readRDS(file.path(cached.path,"optics","dffl.rds"))
-  dfabs_filtered <- dfabs[,c("nm",unique(df_regular$CAGRnumber))]
-  dffl_filtered <- dffl[,c("exem",unique(df_regular$CAGRnumber))]
+  saveRDS(df_regular, file = file.path(cached.path,"final","rds","summaryNoWW_noQA.rds"))
+  saveRDS(df_WW, file = file.path(cached.path,"final","rds","summaryWW_noQA.rds"))
+  saveRDS(df_QA, file = file.path(cached.path,"final","rds","summaryQA.rds"))
+
+  filterOptics(df_regular, dfabs, dffl, "All_noWW_noQA", cached.path)
   
-  write.csv(dfabs_filtered, file = file.path(cached.path,"final","dfabs.csv"), row.names = FALSE)
-  write.csv(dffl_filtered, file = file.path(cached.path,"final","dffl.csv"), row.names = FALSE)
+  filterOptics(df_WW, dfabs, dffl, "_WW_noQA", cached.path)
   
-  wwGR <- unique(df_WW$CAGRnumber)
-  wwGR <- wwGR[wwGR != "x"]
-  dfabs_filtered <- dfabs[,c("nm",wwGR)]
-  dffl_filtered <- dffl[,c("exem",wwGR)]
-  
-  write.csv(dfabs_filtered, file = file.path(cached.path,"final","dfabs_WW.csv"), row.names = FALSE)
-  write.csv(dffl_filtered, file = file.path(cached.path,"final","dffl_WW.csv"), row.names = FALSE)
-  
-  qaGR <- unique(df_QA$CAGRnumber)
-  qaGR <- qaGR[qaGR != "N/A"]
-  dfabs_filtered <- dfabs[,c("nm",qaGR)]
-  dffl_filtered <- dffl[,names(dffl) %in% c("exem",qaGR)]
-  
-  write.csv(dfabs_filtered, file = file.path(cached.path,"final","dfabs_QA.csv"), row.names = FALSE)
-  write.csv(dffl_filtered, file = file.path(cached.path,"final","dffl_QA.csv"), row.names = FALSE)
-  
-  
+  filterOptics(df_QA, dfabs, dffl, "_QA", cached.path)
+
 }
 
 filterData(cached.path)
