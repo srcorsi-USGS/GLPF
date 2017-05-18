11be2471ed8007dc7f9166f81b5855f7ef13d3d7
Steve Corsi <srcorsi@usgs.gov>
2017-05-18 13:40:37 -0500

    Added second grouping variable to make less groups with more observations per group. Modifying PCA to account for this

--- scripts/2_process/eventGrouping.R
+++ scripts/2_process/eventGrouping.R
@@ -8,12 +8,17 @@ eventGroups <- function(cached.path, base.name, cached.save){
   summaryDF <- readRDS(file.path(cached.path,"8_process_new_categories","rds",paste0("summary",base.name,".rds")))
   groupData <- read.csv(file.path(cached.path,"8_process_new_categories","eventFreqAndDatesWGroups.csv"),stringsAsFactors = FALSE)
   
+  summaryDF <- summaryDF[,-c(which(names(summaryDF) %in% c("eventGroup","eventGroup2")))]
   eventGroups <- groupData[,"EventGroup"]
+  eventGroups2 <- groupData[,"EventGroup2"]
   names(eventGroups) <- groupData[,"eventNum"]
+  names(eventGroups2) <- groupData[,"eventNum"]
   summaryDF$eventGroup <- eventGroups[summaryDF$eventNum]
+  summaryDF$eventGroup2 <- eventGroups2[summaryDF$eventNum]
   
   cr <- which(names(summaryDF)=="sources2")
-  summaryDF <- summaryDF[,c(1:cr,ncol(summaryDF),(cr+1):(ncol(summaryDF)-1))]
+  summaryDF <- summaryDF[,c(1:cr,c((ncol(summaryDF)-1),ncol(summaryDF)),
+                            (cr+2):(ncol(summaryDF)-2))]
   
   # dir.create(file.path(cached.path,cached.save), showWarnings = FALSE)
   # dir.create(file.path(cached.path,cached.save,"rds"), showWarnings = FALSE)
@@ -27,4 +32,3 @@ cached.path <- "cached_data"
 base.name <- "_noWW_noQA"
 cached.save <- "8_process_new_categories"
 eventGroups(cached.path, base.name, cached.save)
-base.name <- "_noWW_noQA"
--- scripts/4_model/EventGroupPCA.R
+++ scripts/4_model/EventGroupPCA.R
@@ -12,8 +12,10 @@ df.orig <- readRDS("./cached_data/8_process_new_categories/rds/summary_noWW_noQA
 dfGroups <- read.csv("./cached_data/8_process_new_categories/eventFreqAndDatesWGroups.csv",stringsAsFactors = FALSE)
 df <- df.orig
 
-beginIV <- "OB1"
-endIV <- "logSn.9"
+groupVar <- 
+
+beginIV <- "Sag240_255"
+endIV <- "rBS44_S45_BF"
 begin <- which(names(df)==beginIV)
 end <- which(names(df)==endIV)
 IVs <- names(df)[begin:end]
@@ -27,8 +29,11 @@ rmCols <- unique(which(names(df) %in% c(na.info.list$na.cols.partial,
                                         na.info.list$inf.cols)))
 dfrmCols <- df[,-rmCols]
 dfRemoved <- df[rmRows,]
-df <- df[-rmRows,]
+df <- df[-rmCols,]
 
+begin <- which(names(df)==beginIV)
+end <- which(names(df)==endIV)
+IVs <- names(df)[begin:end]
 x <- as.matrix(df[,IVs])
 
 pca <-prcomp(x,center = TRUE, scale=TRUE) 
@@ -37,22 +42,28 @@ cexpoints <- 1
 xlabel <- "Component 1"
 ylabel <- "Component 2"
 
-groupObs <- group_by(dfGroups,EventGroup) %>%
-  summarise(groupObs = sum(Freq))
-which(groupObs$groupObs > 40)
+# groupObs <- group_by(dfGroups,EventGroup) %>%
+#   summarise(groupObs = sum(Freq))
+# which(groupObs$groupObs > 40)
 
 #Compute distance between mean of groups: use MMSD PAH Euclidian distance analysis as example
 
-groups <- unique(dfGroups$EventGroup)
+groups <- unique(dfGroups[,groupVar])
 
 colorOptions <- brewer.pal(9, "Set1")
 
 
-filenm <- "PCAEventGroup.pdf"
+filenm <- "PCAEventGroupRatios2.pdf"
 pdf(filenm)
+par(mar=c(0,0,0,0),oma=c(6,6,3,2),mfrow=c(3,4))
+State <- ""
 for (i in 1:length(groups)){
-  
-  subdf <- dfGroups[which(dfGroups$EventGroup==groups[i]),]
+  if(State != substr(groups[i],1,1)){
+  par(mar=c(0,0,0,0),oma=c(6,6,3,2),mfrow=c(3,4))
+  State <- substr(groups[i],1,1)
+  }
+    
+  subdf <- dfGroups[which(dfGroups[,groupVar]==groups[i]),]
   events <- unique(subdf[,"eventNum"])
   hydroCond <- unique(subdf[,"eventHydroCond"])
 #  eventSub <- dfGroups[which(dfGroups$EventGroup==groups[i]),"eventNum"]
@@ -61,15 +72,17 @@ for (i in 1:length(groups)){
   # names(eventColors) <- events
   for(j in 1:length(events))plotColors <- ifelse(df$eventNum==events[j],colorOptions[j],plotColors)
 
-  
-
   #plotColors[which(df$eventNum %in% eventSub)] <- "blue"
   
-  plot(pca$x[,1],pca$x[,2], xlab=xlabel, ylab=ylabel, 
-       cex=cexpoints,col="grey",pch=20,main=paste(groups[i],",",hydroCond))   # this puts a small point at the center
+  plot(pca$x[,1],pca$x[,2], xlab="", ylab="", xaxt="n",yaxt="n",
+       cex=cexpoints,col="grey",pch=20)   # 
   points(pca$x[,1],pca$x[,2],  
-       cex=cexpoints,col=plotColors,pch=20)   # this puts a small point at the center
+       cex=cexpoints,col=plotColors,pch=20)   # 
+  text(max(pca$x[,1]*0.8),max(pca$x[,2]),paste(groups[i],",",hydroCond),cex=0.8)
 }
 
 dev.off()
 shell.exec(filenm)
+
+unique(plotColors)
+
